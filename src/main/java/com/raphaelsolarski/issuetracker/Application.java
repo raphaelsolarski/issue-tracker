2a3153b5bbddc581512bd8c33b035573c6559549
raphaelsolarski <solarski.rafal@gmail.com>
2016-04-11 19:40:44 +0200

    Initial commit.

---
+++ src/main/java/com/raphaelsolarski/issuetracker/Application.java
@@ -0,0 +1,29 @@
+package com.raphaelsolarski.issuetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.raphaelsolarski.issuetracker")
+@EnableSwagger2
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2);
+    }
+}
